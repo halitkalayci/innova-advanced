b9226fb69dde8560d32b299ab98cd0f8330012be
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-10-23 10:16:41 +0300

    gateway

---
+++ order-service/src/main/java/com/innova/order_service/api/controllers/OrdersController.java
@@ -0,0 +1,14 @@
+package com.innova.order_service.api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/orders")
+public class OrdersController {
+  @GetMapping
+  public String get() {
+    return "Order service working..";
+  }
+}
