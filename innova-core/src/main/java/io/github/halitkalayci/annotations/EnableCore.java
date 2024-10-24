db12bd4e688aefb291d34d059047d5afd06b02ef
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-10-24 11:51:01 +0300

    latest

--- innova-core/src/main/java/io/github/halitkalayci/annotations/EnableCore.java
+++ innova-core/src/main/java/io/github/halitkalayci/annotations/EnableCore.java
@@ -1,6 +1,8 @@
 package io.github.halitkalayci.annotations;
 
+import io.github.halitkalayci.jwt.BaseJwtService;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
